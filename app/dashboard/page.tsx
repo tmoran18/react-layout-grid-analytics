685a1020664c4de03bf8c711c222a5858fd19dd8
tmoran18 <support@tim-moran.com>
2025-03-29 14:57:21 +1000

    fix build error

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,11 +1,7 @@
 import { AppSidebar } from "@/components/app-sidebar";
 import { ChartGrid } from "@/components/chart-grid";
-import { DataTable } from "@/components/data-table";
-import { SectionCards } from "@/components/section-cards";
 import { SiteHeader } from "@/components/site-header";
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
-
-import data from "./data.json";
 
 export default function Page() {
   return (
